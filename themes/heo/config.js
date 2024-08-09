82c628c6008ca33a90f25b41f75bc02b7bc00278
lllhjf <lllhjf@jaist.ac.jp>
2024-08-09 13:16:23 +0800

    关闭下一篇推荐

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -127,8 +127,8 @@ const CONFIG = {
   HEO_POST_LIST_PREVIEW: false, // 读取文章预览
   HEO_POST_LIST_IMG_CROSSOVER: true, // 博客列表图片左右交错
 
-  HEO_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
-  HEO_ARTICLE_COPYRIGHT: true, // 显示文章版权声明
+  HEO_ARTICLE_ADJACENT: false, // 显示上一篇下一篇文章推荐
+  HEO_ARTICLE_COPYRIGHT: false, // 显示文章版权声明
   HEO_ARTICLE_RECOMMEND: true, // 文章关联推荐
 
   HEO_WIDGET_LATEST_POSTS: true, // 显示最新文章卡
