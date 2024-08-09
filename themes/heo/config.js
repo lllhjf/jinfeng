dcfbd6ac70f9bc98f9d29087af72ce0c74ad7d3d
lllhjf <lllhjf@jaist.ac.jp>
2024-08-09 13:18:02 +0800

    更新个人信息卡片栏

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -15,10 +15,10 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '',
-  HEO_HERO_TITLE_2: '',
-  HEO_HERO_TITLE_3: '',
-  HEO_HERO_TITLE_4: '',
+  HEO_HERO_TITLE_1: 'Welcome to my homepage!',
+  HEO_HERO_TITLE_2: 'Welcome to my homepage!',
+  HEO_HERO_TITLE_3: 'Welcome to my homepage!',
+  HEO_HERO_TITLE_4: 'Welcome to my homepage!',
   HEO_HERO_TITLE_5: 'Welcome to my homepage!',
   HEO_HERO_TITLE_LINK: 'https://minei.eu.org',
 
@@ -34,7 +34,7 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    'Hello！I'm',
+    'Hello！I'm'
   ],
 
   // 个人资料底部按钮
