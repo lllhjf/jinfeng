ee9db795e2e242b91eca1f810cfbc5b072d9a0ad
lllhjf <lllhjf@jaist.ac.jp>
2024-08-09 11:42:58 +0800

    修改菜单按钮的颜色为紫色

--- themes/heo/components/MenuItemDrop.js
+++ themes/heo/components/MenuItemDrop.js
@@ -39,7 +39,7 @@ export const MenuItemDrop = ({ link }) => {
             return (
               <li
                 key={index}
-                className='cursor-pointer hover:bg-blue-600 dark:hover:bg-yellow-600 hover:text-white text-gray-900 dark:text-gray-100  tracking-widest transition-all duration-200 py-1 pr-6 pl-3'>
+                className='cursor-pointer hover:bg-[#845EC2] dark:hover:bg-[#845EC2] hover:text-white text-gray-900 dark:text-gray-100  tracking-widest transition-all duration-200 py-1 pr-6 pl-3'>
                 <Link href={sLink.href} target={link?.target}>
                   <span className='text-sm text-nowrap font-extralight'>
                     {link?.icon && <i className={sLink?.icon}> &nbsp; </i>}
